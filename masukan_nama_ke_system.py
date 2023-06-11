8ea616ab8cd6f32a7d9687bec0f4c9206a34912b
Dimas Ardiminda Edia Putra <113449449+DimzsArdiminda@users.noreply.github.com>
2023-06-11 21:46:06 +0700

    Update masukan_nama_ke_system.py

--- masukan_nama_ke_system.py
+++ masukan_nama_ke_system.py
@@ -1,35 +1,37 @@
-#  * 1. meminta user untuk memasukan berapa jumlah murid, digunkan untuk mengatur berapa kali iterasi berjalan
-#  * 2. menerima nama murid dan nilai dari user dan memasukannya ke dalam array
-#  * 3. menggabungkan 2 tipe data (Nama (str) Nilai (int)) 
-#  * 4. menampilkan setipa data yang ada
-
-# menggabungkan 2 tipe data, nilai dan nama 
 def gabungNamaNilai(nama, nilai):
-    return list(zip(nama,nilai))
-
-def tampilkanData(data):
-    for nama, nilai in data:
-        print(f"Nama {nama} nilai {nilai}")
+    return list(zip(nama, nilai))
+# def tampilkanData(data):
+#     for nama, nilai in data:
+#         print(f"Nama {nama} nilai {nilai}")
 
 print("""
-      Selamat datang di Program untuk memasukan nilai Peserta didik ke dalam sebuah variabel \n
+      Selamat datang di Program untuk memasukkan nilai Peserta didik ke dalam sebuah variabel \n
       
-      Keterangan 
-      1. Masukan jumlah murid + 1 misal, jika murid anda sebanyak 40, maka masukan  41\n
-      2. Data murid ke 0 bersifat abstrak (tidak jelas), tetapi harus dan wajib diisi\n
+      Keterangan:
+      1. Masukkan jumlah murid + 1. Misal, jika murid Anda sebanyak 40, maka masukkan 41\n
+      2. Data murid ke-0 bersifat abstrak (tidak jelas), tetapi harus dan wajib diisi\n
       """)
 
-# step 1 meminta jumlah murid
-jumlah_murid = int(input("masukan jumlah murid: "))
-# steo 2  melaukakn perulangan sesuai jumlah murid dan memasukaknnya ke dalam array
+# Step 1: Meminta jumlah murid
+jumlah_murid = int(input("Masukkan jumlah murid: "))
+
+# Step 2: Melakukan perulangan sesuai jumlah murid dan memasukkannya ke dalam array
 nama_murid = []
 nilai_murid = []
 for i in range(jumlah_murid):
-    print(f"\ndata murid ke {i}")
-    nama = input("Masukan Nama murid: ")
-    nilai = int(input("Masukan Nilai Murid: "))
+    print(f"\nData murid ke-{i+1}")
+    nama = input("Masukkan Nama murid: ")
+    nilai = int(input("Masukkan Nilai Murid: "))
     nama_murid.append(nama)
     nilai_murid.append(nilai)
 
+# Step 3: Menggabungkan data nama dan nilai
 data_murid = gabungNamaNilai(nama_murid, nilai_murid)
-tampilkanData(data_murid)
+
+# Step 4: Menampilkan data yang ada
+# tampilkanData(data)
+
+for nama, nilai in data_murid:
+    print(f"Nama {nama}, nilai {nilai}")
+    
+    
