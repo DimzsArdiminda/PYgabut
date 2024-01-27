5436ec4af0853db587206d8fc32afbaac596e1ad
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2024-01-28 06:00:03 +0700

    Update PencarianData.py

--- PencarianData.py
+++ PencarianData.py
@@ -38,3 +38,25 @@ range_values = max_value - min_value +1
 
 # membuat list kosong
 count = [0] * range_values
+
+# menghitung frekuensi data setiap num in numbers
+# simpang dalam list kosong
+for num in numb:
+    count[num - min_value] +=1
+
+# menghitung akumulasi jumlah list yang di butuhkan
+for i in range(1, range_values):
+    count[i] += count[i-1]
+
+# membuat list baru
+sorted_numbers = [0] * len(numb)
+
+# pengururtan (memanfaatkan perulangan, kumpulan data, frek komulatif)
+for num in numbers:
+  index = count[num - min_value] - 1
+  sorted_numbers[index] = num
+  count[num - min_value] -= 1
+
+print("Data sebelum diurutkan:", numbers)
+print("Data setelah diurutkan:", sorted_numbers)
+    
